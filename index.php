d619d08e20659b1bdbf9e321a431aa5a172b7eba
Mateusz Ostróżka <10376260u@qed.local>
2025-01-14 12:40:56 +0100

    Przesylanie pliku

--- index.php
+++ index.php
@@ -1,18 +1,12 @@
-<?php
-
-header('Content type: text/html; charset=UTF-8');
-
-?>
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>File Uploder</title>
+        <title>File Uploader</title>
     </head>
 
     <body>
 
-    <h1>File Uploder</h1>
+    <h1>File Uploader</h1>
     <form action="" method="post" enctype="multipart/form-data">
         <fieldset>
             <label for="file">
@@ -29,6 +23,25 @@ header('Content type: text/html; charset=UTF-8');
 
 function prepareFilename($filename){
     $result = mb_strtolower($filename,'UTF-8');
-    $result = str_replace(array('ą','ę','ć'),array('a','e','c'),$result);
-    $result = str_replace(' ','_',$result)
+    $result = str_replace(array('ą','ę','ć','ś','ż'),array('a','e','c','s','z'),$result);
+    $result = str_replace(' ','_',$result);
+    return $result;
 }
+
+$uploadDir = 'upload/';
+
+if (!file_exists($uploadDir)){
+    mkdir($uploadDir, 0777, true);
+}
+
+if (isset($_FILES['file'])) {
+    $filename = prepareFilename($_FILES['file']['name']);
+    $destination = $uploadDir . $filename;
+
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $destination)) {
+        echo "Plik został pomyślnie przesłany.";
+    } else {
+        echo "Wystąpił błąd podczas przesyłania pliku.";
+    }
+}
+?>
